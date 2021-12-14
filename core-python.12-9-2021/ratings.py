5b52c9f4061e4151f8161bc7fd568ac2081d59a6
Emily Simpson <emilousim@gmail.com>
2021-12-14 17:02:46 -0500

    created repo for Data Solutions Exercises

---
+++ core-python.12-9-2021/ratings.py
@@ -0,0 +1,29 @@
+"""Restaurant rating lister."""
+# put your code here
+file = open("scores.txt")
+d = {}
+for line in file:
+    x = line.rstrip("\n").split(":")
+    a = x[0]
+    b = x[1]
+    d[a] = b
+# print(d)
+sortedList = (sorted(d.items()))
+
+print(sortedList)
+
+newRest = ""
+newRest = str(input("Enter another restaurant!: "))
+
+newRestRating = ""
+newRestRating = int(input("Rate it, 1-5: "))
+
+d.update({newRest: newRestRating})
+
+print(sorted(d.items()))
+
+
+# the first step we take is importing the file into this file by using the code in line 3. we use the open function to
+# open and read the content. second step = (d={}), this creates an empty dictionary to put our key/value pairs in.
+# .rstrip takes the \n off and .split slices everything into their key and value pair. once that's done, we just use the
+# sorted function which sorts by key, alphabetically.
