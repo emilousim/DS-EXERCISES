69472912a651f1bb0ca3128488aa16111808c0d8
Emily Simpson <emilousim@gmail.com>
2021-12-19 16:45:45 -0500

    all functions working properly

--- py-data-structures.12-9-2021/cohort_data.py
+++ py-data-structures.12-9-2021/cohort_data.py
@@ -1,22 +1,8 @@
 """Functions to parse a file containing student data."""
-
 from os import dup
-
 
 def all_houses(filename):
     # """Return a set of all house names in the given file.
-
-    # For example:
-    #   >>> unique_houses('cohort_data.txt')
-    #   {"Dumbledore's Army", 'Gryffindor', ..., 'Slytherin'}
-
-    # Arguments:
-    #   - filename (str): the path to a data file
-
-    # Return:
-    #   - set[str]: a set of strings
-    # """
-
     houses = set()
 
     cohort_data = open(filename)
@@ -30,32 +16,7 @@ def all_houses(filename):
 
 
 def students_by_cohort(filename, cohort='All'):
-    """Return a list of students' full names by cohort.
-
-    Names are sorted in alphabetical order. If a cohort isn't
-    given, return a list of all students. For example:
-      >>> students_by_cohort('cohort_data.txt')
-      ['Adrian Pucey', 'Alicia Spinnet', ..., 'Zacharias Smith']
-
-      >>> students_by_cohort('cohort_data.txt', cohort='Fall 2015')
-      ['Angelina Johnson', 'Cho Chang', ..., 'Terence Higgs', 'Theodore Nott']
-
-      >>> students_by_cohort('cohort_data.txt', cohort='Winter 2016')
-      ['Adrian Pucey', 'Andrew Kirke', ..., 'Roger Davies', 'Susan Bones']
-
-      >>> students_by_cohort('cohort_data.txt', cohort='Spring 2016')
-      ['Cormac McLaggen', 'Demelza Robins', ..., 'Zacharias Smith']
-
-      >>> students_by_cohort('cohort_data.txt', cohort='Summer 2016')
-      ['Alicia Spinnet', 'Dean Thomas', ..., 'Terry Boot', 'Vincent Crabbe']
-
-    Arguments:
-      - filename (str): the path to a data file
-      - cohort (str): optional, the name of a cohort
-
-    Return:
-      - list[list]: a list of lists
-    """
+    #Return a list of students' full names by cohort.
 
     students = []
     
@@ -70,35 +31,7 @@ def students_by_cohort(filename, cohort='All'):
 
 
 def all_names_by_house(filename):
-    """Return a list that contains rosters for all houses, ghosts, instructors.
-
-    Rosters appear in this order:
-    - Dumbledore's Army
-    - Gryffindor
-    - Hufflepuff
-    - Ravenclaw
-    - Slytherin
-    - Ghosts
-    - Instructors
-
-    Each roster is a list of names sorted in alphabetical order.
-
-    For example:
-      >>> rosters = hogwarts_by_house('cohort_data.txt')
-      >>> len(rosters)
-      7
-
-      >>> rosters[0]
-      ['Alicia Spinnet', ..., 'Theodore Nott']
-      >>> rosters[-1]
-      ['Filius Flitwick', ..., 'Severus Snape']
-
-    Arguments:
-      - filename (str): the path to a data file
-
-    Return:
-      - list[list]: a list of lists
-    """
+    #Return a list that contains rosters for all houses, ghosts, instructors.
 
     dumbledores_army = []
     gryffindor = []
@@ -141,23 +74,7 @@ def all_names_by_house(filename):
             sorted(instructors),]
 
 def all_data(filename):
-    """Return all the data in a file.
-
-    Each line in the file is a tuple of (full_name, house, advisor, cohort)
-
-    Iterate over the data to create a big list of tuples that individually
-    hold all the data for each person. (full_name, house, advisor, cohort)
-
-    For example:
-      >>> all_student_data('cohort_data.txt')
-      [('Harry Potter', 'Gryffindor', 'McGonagall', 'Fall 2015'), ..., ]
-
-    Arguments:
-      - filename (str): the path to a data file
-
-    Return:
-      - list[tuple]: a list of tuples
-    """
+    #Return all the data in a file.
 
     all_data = []
 
@@ -172,26 +89,7 @@ def all_data(filename):
 
 
 def get_cohort_for(filename, name):
-    """Given someone's name, return the cohort they belong to.
-
-    Return None if the person doesn't exist. For example:
-      >>> get_cohort_for('cohort_data.txt', 'Harry Potter')
-      'Fall 2015'
-
-      >>> get_cohort_for('cohort_data.txt', 'Hannah Abbott')
-      'Winter 2016'
-
-      >>> get_cohort_for('cohort_data.txt', 'Someone else')
-      None
-
-    Arguments:
-      - filename (str): the path to a data file
-      - name (str): a person's full name
-
-    Return:
-      - str: the person's cohort or None
-    """
-
+    #Given someone's name, return the cohort they belong to. 
     for full_name, _, _, cohort_name in all_data(filename):
       if (full_name) == name:
         return cohort_name
@@ -199,19 +97,7 @@ def get_cohort_for(filename, name):
 
 
 def find_duped_last_names(filename):
-    """Return a set of duplicated last names that exist in the data.
-
-    For example:
-      >>> find_name_duplicates('cohort_data.txt')
-      {'Creevey', 'Weasley', 'Patil'}
-
-    Arguments:
-      - filename (str): the path to a data file
-
-    Return:
-      - set[str]: a set of strings
-    """
-
+    #Return a set of duplicated last names that exist in the data.
     duplicates = set()
     checked = set()
     for full_name, _, _, _ in all_data(filename):
@@ -226,16 +112,7 @@ def find_duped_last_names(filename):
 
 
 def get_housemates_for(filename, name):
-    """Return a set of housemates for the given student.
-
-    Given a student's name, return a list of their housemates. Housemates are
-    students who belong to the same house and were in the same cohort as the
-    given student.
-
-    For example:
-    >>> get_housemates_for('cohort_data.txt', 'Hermione Granger')
-    {'Angelina Johnson', ..., 'Seamus Finnigan'}
-    """
+    #Return a set of housemates for the given student.
 
     housemates = set()
 
