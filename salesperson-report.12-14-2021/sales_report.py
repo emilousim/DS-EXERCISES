f77a85c55de9ac30e2e68dcceb0b0458b31dfc25
Emily Simpson <emilousim@gmail.com>
2021-12-30 13:38:29 -0500

    added comments explaining what each line of code does, and added some ways to simplify the process.

--- salesperson-report.12-14-2021/sales_report.py
+++ salesperson-report.12-14-2021/sales_report.py
@@ -1,25 +1,33 @@
 """Generate sales report showing total melons each salesperson sold."""
 
+salespeople = [] #initiates salespeople variable
+melons_sold = [] #initiates melons_sold variable
 
-salespeople = []
-melons_sold = []
-
-f = open('sales-report.txt')
+f = open('sales-report.txt') #opens and reads file 
 for line in f:
     line = line.rstrip()
     entries = line.split('|')
 
-    salesperson = entries[0]
-    melons = int(entries[2])
+#lines 7-9 splits the info by each | divider, also strips them of whitespace. 
+# this could be simplified by writing it as follows:
+#  df = pd.read_csv("sales-report.txt", sep="|") which would do the same thing and would put the info in a neat dataframe format 
+  
+    salesperson = entries[0] # sets the salesperson variable as the first index value in entries
+    melons = int(entries[2]) # sets the melons variable as the third index value in entries, converted to an integer.
 
-    if salesperson in salespeople:
-        position = salespeople.index(salesperson)
+#lines 15-16 could be simplified by defining columns in the dataframe, like this: df.columns = ([salesperson], [revenue], [NumOfMelons])
 
-        melons_sold[position] += melons
+    if salesperson in salespeople: #if the value in entries[0] is in the salespeople [],
+        position = salespeople.index(salesperson) #.index gives us a way to identify specific rows
+
+        melons_sold[position] += melons #melons_sold[whoever the sales person is], add a melon for each sale 
     else:
-        salespeople.append(salesperson)
-        melons_sold.append(melons)
-
+        salespeople.append(salesperson) #add that salesperson to the df
+        melons_sold.append(melons) #add their melon tally to the melons_sold
 
 for i in range(len(salespeople)):
-    print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+    print(f'{salespeople[i]} sold {melons_sold[i]} melons') #print whatever salespersons' name sold however many melons they sold
+
+#calculating the melons sold by each person could be simplified by:
+# df.groupby(["salesperson"]).sum().sort_values("NumOfMelons", ascending=False)
+
